648a4eac351321c6cfa9f75cda7e9850b0d0029f
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2023-08-16 21:15:11 -0400

    add start screen fields

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,14 +5,47 @@ const bootstrap = require("bootstrap");
 document.body.style.backgroundColor = "Cornsilk";
 
 function App() {
+  const [startScreen, setStartScreen] = useState(true);
+  const [firstName, setFirstName] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(false);
+
   return (
-    <div>
+    <div class="pt-5">
       <div class="container px-4 text-center pt-5">
         <h1 class="shadow-lg p-3 mb-5 bg-body-tertiary rounded display-1">
           Location Sharing
         </h1>
       </div>
-      <div></div>
+      {startScreen && (
+        <div class="container px-4 text-center pt-5">
+          <div class="row justify-content-md-center">
+            <div class="col col-lg-3">
+              <div class="form-floating">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="floatingInputGrid"
+                  placeholder="Enter your first name..."
+                  value={firstName}
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                  }}
+                />
+                <label for="floatingInputGrid">Enter your first name...</label>
+              </div>
+            </div>
+          </div>
+          <div class="pt-5">
+            <button
+              type="button"
+              class="btn btn-success btn-lg"
+              disabled={btnDisabled}
+            >
+              Connect
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
