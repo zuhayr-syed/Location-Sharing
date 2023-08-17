f2f26aa7e165bf5869128f5ce70aa5593c3850ba
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2023-08-16 20:39:24 -0400

    remove css and create template

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+
+const bootstrap = require("bootstrap");
+
+document.body.style.backgroundColor = "Cornsilk";
 
 function App() {
-  return <div className="App">yeoooooooooo</div>;
+  return (
+    <div>
+      <div class="container px-4 text-center pt-5">
+        <h1 class="shadow-lg p-3 mb-5 bg-body-tertiary rounded display-1">
+          Location Sharing
+        </h1>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
