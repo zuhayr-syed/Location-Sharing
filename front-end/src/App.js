61c4338c93c65d151c88002ebc1d117f50fb7c74
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2023-08-17 20:20:57 -0400

    add coordinate table

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,17 @@ const bootstrap = require("bootstrap");
 
 document.body.style.backgroundColor = "Cornsilk";
 
+const coords = [
+  { Latitude: 32.223, Longitude: -72.54 },
+  { Latitude: 52.223, Longitude: -84.54 },
+  { Latitude: 12.223, Longitude: -63.54 },
+];
+const coords2 = [
+  { Latitude: 22.223, Longitude: -62.54 },
+  { Latitude: 12.223, Longitude: -94.54 },
+  { Latitude: 42.223, Longitude: -83.54 },
+];
+
 function App() {
   const [startScreen, setStartScreen] = useState(true);
   const [firstName, setFirstName] = useState("");
@@ -109,6 +120,25 @@ function App() {
                     <p class="fs-2 shadow-sm p-3 mb-5 bg-body-tertiary rounded display-1">
                       Your Coordinates
                     </p>
+                    <p class="fs-3">
+                      <span class="p-5">
+                        Latitude: {coords.toReversed()[0].Latitude}
+                      </span>
+                      <span>Longitude: {coords.toReversed()[0].Longitude}</span>
+                    </p>
+                    <div
+                      class="shadow-sm p-3 bg-body-tertiary rounded"
+                      style={{ height: "50vh" }}
+                    >
+                      <ul class="list-group">
+                        {coords.toReversed().map((item) => (
+                          <li class="list-group-item">
+                            <span class="p-5">Latitude: {item.Latitude}</span>
+                            <span>Longitude: {item.Longitude}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
                 <div class="col">
@@ -117,6 +147,27 @@ function App() {
                     <p class="fs-2 shadow-sm p-3 mb-5 bg-body-tertiary rounded display-1">
                       {otherName}
                     </p>
+                    <p class="fs-3">
+                      <span class="p-5">
+                        Latitude: {coords2.toReversed()[0].Latitude}
+                      </span>
+                      <span>
+                        Longitude: {coords2.toReversed()[0].Longitude}
+                      </span>
+                    </p>
+                    <div
+                      class="shadow-sm p-3 bg-body-tertiary rounded"
+                      style={{ height: "50vh" }}
+                    >
+                      <ul class="list-group">
+                        {coords2.toReversed().map((item) => (
+                          <li class="list-group-item">
+                            <span class="p-5">Latitude: {item.Latitude}</span>
+                            <span>Longitude: {item.Longitude}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
