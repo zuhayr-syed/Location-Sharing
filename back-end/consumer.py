bf4d816827d22036122aa01532e0cbd0473ff2ec
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2023-08-23 23:28:53 -0400

    implement database creation

---
+++ back-end/consumer.py
@@ -0,0 +1,72 @@
+from kafka import KafkaConsumer
+import json
+import psycopg2
+
+hostname = 'localhost'
+database = 'coordinates'
+username = 'postgres'
+pwd = 'admin'
+port_id = 5432
+
+SERVER_KAFKA_TOPIC = "user_coordinates"
+
+consumer = KafkaConsumer(
+    SERVER_KAFKA_TOPIC,
+    bootstrap_servers="localhost:29092"
+)
+
+conn = None
+cur = None
+
+if __name__ == "__main__":
+    try:
+        conn = psycopg2.connect(
+            host=hostname,
+            dbname=database,
+            user=username,
+            password=pwd,
+            port=port_id
+        )
+        cur = conn.cursor()
+        print("Connected to database")
+
+        print("Consumer listening...")
+        itr = 0
+        track = 0
+        create_db = True
+        while True:
+            for message in consumer:
+                if create_db:
+                    create_db = False
+                    print(f"Created database session{str(itr)}")
+                    create_script = ''' CREATE TABLE IF NOT EXISTS session{} (
+                                            id  SERIAL PRIMARY KEY,
+                                            user_name    VARCHAR(40) NOT NULL,
+                                            coord_id    INT NOT NULL,
+                                            user_coord_id   INT NOT NULL,
+                                            latitude    INT,
+                                            longitude   INT
+                                    ) '''.format(str(itr))
+                    cur.execute(create_script)
+                    conn.commit()
+
+                print("Reading data...")
+                consumed_message = json.loads(message.value.decode())
+                print(f"Consumed message: {str(consumed_message)}")
+
+                if 'alert' in consumed_message:
+                    track += 1
+                    if track > 1:
+                        track = 0
+                        itr += 1
+                        create_db = True
+                        print("Current session has ended")
+
+    except Exception as error:
+        print(error)
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+        print("Disconnected from database")
